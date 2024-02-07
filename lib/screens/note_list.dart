bdd16ae7a8459e223aa6543e2ce853f2740439be
TheRealHex <devilxnp@gmail.com>
2024-02-07 12:18:13 +0545

    first: commit

---
+++ lib/screens/note_list.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({super.key});
+
+  @override
+  State<NoteList> createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  int count = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: _getNoteListView(),
+    );
+  }
+
+  ListView _getNoteListView() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Card(
+            color: Colors.white,
+            elevation: 1.0,
+            child: ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Colors.black87,
+              ),
+              title: const Text('Dummy Title'),
+              subtitle: const Text('Dummy date'),
+              trailing:
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+              onTap: () {
+                debugPrint('test');
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
